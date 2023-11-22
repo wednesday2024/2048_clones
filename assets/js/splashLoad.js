ddfee264b08ec1864b48dc578e571bafd7c6bb20
Ryzen5-3600 <jordanluders@proton.me>
2023-11-22 10:56:14 -0600

    Revert loading times.

--- assets/js/splashLoad.js
+++ assets/js/splashLoad.js
@@ -5,5 +5,5 @@ const splash = document.querySelector('.splash');
 document.addEventListener('DOMContentLoaded', (e)=>{
 	setTimeout(()=>{
 		splash.classList.add('display-none');
-	}, 1000);
+	}, 2000);
 })
